612f12a5c636fc28c8b9c62ec659d4d07d196f59
David Reis <davisreis11@gmail.com>
2025-12-12 21:30:26 -0300

    feat: ícones dos cards de vaga

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,19 @@
-import { FavoriteIcon, FavoriteIconFilled } from '../assets/nav'
+import { FavoriteIcon, FavoriteIconFilled } from '../assets/nav';
+import companyIcon from '../assets/job-applications/company.svg';
+import locationIcon from '../assets/job-applications/location.svg';
+import workModeIcon from '../assets/job-applications/work-mode.svg';
+
 export default function JobOpening({data, type }) {
   return (
     <div className="flex justify-between pb-5 pt-1 border-t-2 w-[95%] relative nth-last-1:border-b-2">
       {type == 'job_opening' ? (
         <div className="flex flex-col">
-          <h3 className="text-sm text-nowrap text-pretty font-bold uppercase">{data.title}</h3>
-          <p className="text-[12px] text-clip"><span></span>{data.company}</p>
-          <p className="text-[12px]"><span></span>{data.location}</p>
-          <p className="text-[12px]"><span></span>{data.work_mode}</p>
+            <h3 className="text-sm text-nowrap text-pretty font-bold uppercase">{data.title}</h3>
+            <div className="flex flex-col gap-2 mt-1">
+              <p className="flex items-end gap-1 text-[12px] text-clip leading-none"><img src={companyIcon}/>{data.company}</p>
+              <p className="flex items-end gap-1 text-[12px] leading-none"><img className="pr-1" src={locationIcon}/>{data.location}</p>
+              <p className="flex items-end gap-1 text-[12px] leading-none"><img className="pr-1px" src={workModeIcon}/>{data.work_mode}</p>
+            </div>
         </div>)
         : (
           <div className="flex flex-col">
