56a4f94ecee95b282a6bf2d84232f945ec17116b
David Reis <davisreis11@gmail.com>
2026-01-05 22:49:49 -0300

    feat: integração com os dados do back-end na página de Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,49 @@
 import { Link } from "react-router-dom";
+import { getMyData } from "../services/api";
+import { useState, useEffect } from "react";
+import {useAuth} from '../context/AuthContext'
+import Loading from "../components/Loading";
 
 export default function Profile(){
+  const {logout,isAuthenticated} = useAuth();
+  const [user,setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error,setError] = useState(null);
+
+  console.log(isAuthenticated);
+
+  useEffect(()=>{
+    const fetchUser = async () =>{
+      try{
+        const response = await getMyData();
+        setUser(response.data);
+      }catch(err){
+        setError(true);
+        console.error(err);
+        if(err.response.status === 403) logout();
+      }finally{
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  },[]);
+
+  
+
+  if(loading) return <Loading></Loading>
+
   return(
     <div className="flex flex-col items-center px-4 lg:px-15">
       <section className="flex flex-col items-center gap-1 border-b-1 w-full pb-5" id="profile-info">
-        <div className="rounded-full border-3">
-          <img src="./src/assets/profile.png" className="w-full h-full rounded-full object-fit" />
+        <div className={`w-40 h-40 rounded-full border-3 bg-[url('../src/assets/profile-placeholder.png')] bg-contain bg-no-repeat bg-center`}>
         </div>
-        <h1 className="font-[Nunito] font-extrabold text-2xl">Marcelo Pires</h1>
-        <div className="flex items-center justify-center bg-gray-300 text-center uppercase font-bold px-5 py-[2px] rounded-full border-2 text-[.65rem]">ele/dele</div>
-        <p className="text-[.6rem] font-bold">21 anos, Fortaleza/CE</p>
+        <h1 className="font-[Nunito] font-extrabold text-2xl">{user.nome}</h1>
+        <div className="flex items-center justify-center bg-gray-300 text-center uppercase font-bold px-5 py-[2px] rounded-full border-2 text-[.65rem]">{user?.pronomes ?? 'seus pronomes'}</div>
+        <p className="text-[.6rem] font-bold">{user?.idade ?? 'Idade'}, {user?.cidade ?? "Cidade"}/{user?.estado ?? "UF"}</p>
       </section>
       <section className="flex flex-col gap-10 items-center w-full py-10" id="buttons">
-        <Link className="text-center w-[90%] max-w-[25rem] py-1 border-2 rounded-full bg-white font-bold uppercase hover:bg-gray-100 ">Meus Dados</Link>
-        <button className="text-center w-[90%] max-w-[25rem] py-1 border-black border-2 rounded-full bg-red-600 font-bold uppercase text-white hover:cursor-pointer hover:bg-red-700">Sair</button>
+        <Link className="text-center w-[90%] max-w-[25rem] py-1 border-2 rounded-full bg-white font-bold uppercase hover:bg-gray-100" to={'/perfil/editar'}>Meus Dados</Link>
+        <button className="text-center w-[90%] max-w-[25rem] py-1 border-black border-2 rounded-full bg-red-600 font-bold uppercase text-white hover:cursor-pointer hover:bg-red-700" onClick={()=>logout()}>Sair</button>
       </section>
     </div>
   );
